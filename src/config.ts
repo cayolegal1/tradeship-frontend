26ecd0f322559909dc3c5034c6f6b6e795939fce
Cayo Legal <cayolegal@hotmail.com>
2025-10-03 02:02:55 -0300

    fix in base url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-export const SERVER_URL = import.meta.env.DEV ? 'http://127.0.0.1:3000' : 'https://tradeship-backend.onrender.com/';
-
+export const SERVER_URL = import.meta.env.DEV
+  ? "http://127.0.0.1:3000"
+  : "https://tradeship-backend.onrender.com";
 
 export const cloudflareTurnstilSiteKey = "0x4AAAAAABkXYVb_SCYSZVp4";
